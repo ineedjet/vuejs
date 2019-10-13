2e8132701383a71e78db4f074acf9070b217a663
Yuriy Kirillov <ykirillov@icloud.com>
2019-10-14 01:16:58 +0300

    add devise client and staff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :clients
+  devise_for :staffs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#index'
+
+  namespace :staff do
+    root 'landing#index'
+  end
+
+  namespace :client do
+    root 'landing#index'
+  end
 end
