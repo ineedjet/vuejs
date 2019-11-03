ccdb23db95259fcbaf5b36ff911c7ad2f796de79
Yuriy Kirillov <yurkakirillov@gmail.com>
2019-11-03 14:56:02 +0300

    fix form_of_ownership column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_011705) do
+ActiveRecord::Schema.define(version: 2019_11_03_115314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2019_11_01_011705) do
 
   create_table "organisations", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "form_of_ownership", null: false
+    t.string "form_of_ownership", null: false
     t.string "inn", null: false
     t.string "ogrn", null: false
     t.datetime "created_at", precision: 6, null: false
